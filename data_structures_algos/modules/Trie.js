3868056d1768dfb88077c9d27540085add0cbbe5
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-04-07 19:17:26 -0700

    Add notes trie

--- data_structures_algos/modules/Trie.js
+++ data_structures_algos/modules/Trie.js
@@ -1,3 +1,33 @@
+/* 
+
+https://leetcode.com/problems/implement-trie-prefix-tree/solution/
+Visualize a trie as a 26 by n matrix with one way relation between the letters in different levels. n is the length of the longest word in the trie.
+
+words — deed, nom, noon
+
+a  b  c  d  e  f  g  h  i  j  k  l  m  n  o ...
+           ↘                            ↘ 
+a  b  c  d  e  f  g  h  i  j  k  l  m  n  o ...
+            ↓                           ↙ ↓
+            ↓                         ↙   ↓
+a  b  c  d  e  f  g  h  i  j  k  l  m  n  o ...
+          ↙                             ↙
+a  b  c  d  e  f  g  h  i  j  k  l  m  n  o ...
+
+The root would look like:
+
+const trie =
+{
+  d: { e: { e: { d: { isWord: true } } } },
+  n: { o: { o: { n: { isWord: true } },
+       m: { isWord: true } }
+     }
+}
+
+*/
+
+// We'll assume that the words provided will consist of letters only and are always lowercase
+
 const Trie = (() => {
   let root
 
