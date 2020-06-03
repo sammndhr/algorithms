8eeae251b14293c59d7092d4f15622f6468a9508
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-06-02 18:21:37 -0700

    Add selection sort

---
+++ sorting/00_selection_sort.js
@@ -0,0 +1,47 @@
+const { swapArrItemsInPlace } = require('../utils')
+
+function selectionSort(arr) {
+  const len = arr.length
+  let currMinIndex
+
+  for (let i = 0; i < len; i++) {
+    currMinIndex = i
+
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[currMinIndex]) {
+        currMinIndex = j
+      }
+    }
+    if (i !== currMinIndex) swapArrItemsInPlace(arr, i, currMinIndex)
+  }
+}
+
+/*
+Time Complexity:
+Best, worst and average case - О(n^2)
+
+Space complexity - O(1)
+*/
+
+/* ---------------------------------------------------------------------------- */
+
+const testCases = [
+  [
+    [5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5]
+  ],
+  [
+    [5, 6, 1, 0, 6, 2],
+    [0, 1, 2, 5, 6, 6]
+  ],
+  [
+    [-1, 6, 2, 100, 0, -11],
+    [-11, -1, 0, 2, 6, 100]
+  ]
+]
+
+for (const test of testCases) {
+  const arr = JSON.parse(JSON.stringify(test[0])) //copy of array
+  selectionSort(arr)
+  console.log(JSON.stringify(arr) === JSON.stringify(test[1]))
+}
