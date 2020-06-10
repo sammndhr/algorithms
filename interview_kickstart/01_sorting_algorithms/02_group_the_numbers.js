02eb351d21760bffa18c97315a27c6140199edda
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-06-09 18:34:04 -0700

    Add group the numbers

---
+++ interview_kickstart/01_sorting_algorithms/02_group_the_numbers.js
@@ -0,0 +1,36 @@
+/* 
+First Intuition: Use two pointers to partition the array like in quicksort
+
+Time Complexity - O(n)
+Space Complexity - O(1)
+*/
+
+function swap(array, i, j) {
+  let temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
+}
+
+function isEven(num) {
+  return num % 2 === 0
+}
+
+function groupTheNumbers(array) {
+  const len = array.length
+
+  let evenPointer = -1,
+    curr = 0
+
+  for (curr; curr < len; curr++) {
+    const element = array[curr]
+    if (isEven(element)) {
+      evenPointer++
+      swap(array, evenPointer, curr)
+    }
+  }
+
+  return array
+}
+
+console.log(groupTheNumbers([1, 2, 3, 5, -1, 0, 7, 8]))
+console.log(groupTheNumbers([1, 2, 3, 4]))
