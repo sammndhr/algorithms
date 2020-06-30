aee0de7b6474d40ee1c4861453b60ef5d46ea59a
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-06-29 21:49:23 -0700

    Add graph first template

--- interview_kickstart/04_graphs_and_its_variants/videos/01_graph_template.js
+++ interview_kickstart/04_graphs_and_its_variants/videos/01_graph_template.js
@@ -1,3 +1,8 @@
+/*
+1. Build the graph:
+Most of the time you'll be given the number of vertices of the graph and the edge list.
+First thing you need to do is build a graph from that information.
+*/
 function buildGraph(n, edges) {
   let adjList = new Array(n).fill().map(() => [])
 
@@ -11,6 +16,11 @@ function buildGraph(n, edges) {
   return adjList
 }
 
+/*
+2. Graph Traversal
+You'll then have to traverse the graph updating a 'visited' array for nodes that's been seen.
+dfs and bfs are two graph traversals that can be used.
+*/
 function dfs(node, visited, adjList) {
   visited[node] = 1
 
@@ -23,11 +33,12 @@ function bfs(source, visited, adjList) {
   const queue = []
   queue.push(source)
   visited[source] = 1
-
   let node
+
   while (queue.length) {
     // Time complexity of queue.shift() is NOT O(1). Implement queue with linked list for O(1)
     node = queue.shift()
+
     for (const neighbor of adjList[node]) {
       if (visited[neighbor] === -1) {
         visited[neighbor] = 1
@@ -37,28 +48,20 @@ function bfs(source, visited, adjList) {
   }
 }
 
-var countComponents = function (n, edges) {
+/* 
+3. Outer Loop
+Because a graph might not be connected, you'll need an outer loop function.
+This function will iterate through the visited array and call dfs/bfs on each node that hasn't been visited.
+*/
+function outerLoop(n, edges) {
   const visited = new Array(n).fill(-1),
     adjList = buildGraph(n, edges)
-  let components = 0
 
   for (let v = 0; v < n; v++) {
     if (visited[v] === -1) {
-      components++
-      // either bfs or dfs works
+      // traverse graph with either bfs or dfs
       // dfs(v, visited, adjList)
       bfs(v, visited, adjList)
     }
   }
-  return components
 }
-
-// Tests
-console.log(countComponents(2, [[1, 0]]))
-console.log(
-  countComponents(5, [
-    [0, 1],
-    [1, 2],
-    [3, 4]
-  ])
-)
