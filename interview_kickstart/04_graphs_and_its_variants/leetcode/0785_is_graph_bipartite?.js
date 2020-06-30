1d89f57b196b6694fa0a2a723132975602c91a29
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-06-30 14:35:29 -0700

    Add Is Graph Bipartite

---
+++ interview_kickstart/04_graphs_and_its_variants/leetcode/0785_is_graph_bipartite?.js
@@ -0,0 +1,79 @@
+// With bfs
+var isBipartite = function (graph) {
+  const adjList = graph,
+    n = graph.length,
+    visited = new Array(n).fill(-1),
+    parent = new Array(n).fill(-1),
+    distance = new Array(n).fill(-1)
+
+  for (let v = 0; v < n; v++) {
+    if (visited[v] === -1) {
+      if (!bfs(v)) return false
+    }
+  }
+
+  return true
+
+  // -----------------------------------------------
+  function bfs(source) {
+    const queue = []
+    queue.push(source)
+
+    visited[source] = 1
+    distance[source] = 0
+
+    while (queue.length) {
+      const node = queue.shift()
+      for (const neighbor of adjList[node]) {
+        if (visited[neighbor] == -1) {
+          visited[neighbor] = 1
+          parent[neighbor] = node
+          distance[neighbor] = distance[node] + 1
+          queue.push(neighbor)
+        } else {
+          // cycle
+          if (parent[node] !== neighbor) {
+            // not bipartite (odd length cycle found)
+            if (distance[node] == distance[neighbor]) return false
+          }
+        }
+      }
+    }
+    return true
+  }
+}
+
+// With dfs
+var isBipartite = function (graph) {
+  const adjList = graph,
+    n = graph.length,
+    visited = new Array(n).fill(-1),
+    parent = new Array(n).fill(-1),
+    color = new Array(n).fill(-1)
+
+  for (let v = 0; v < n; v++) {
+    if (visited[v] === -1) {
+      if (!dfs(v)) return false
+    }
+  }
+
+  return true
+
+  // -----------------------------------------------
+  function dfs(source) {
+    visited[source] = 1
+
+    if (parent[source] === -1) color[source] = 0
+    else color[source] = 1 - color[parent[source]]
+
+    for (const neighbor of adjList[source]) {
+      if (visited[neighbor] === -1) {
+        parent[neighbor] = source
+        if (!dfs(neighbor)) return false
+      } else {
+        if (color[source] === color[neighbor]) return false
+      }
+    }
+    return true
+  }
+}
