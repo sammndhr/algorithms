44f4f0eb1a51a1a67ce144876710b51645246b9e
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-06-30 18:10:27 -0700

    Add max area of island

---
+++ interview_kickstart/04_graphs_and_its_variants/leetcode/0695_max_area_of_island.js
@@ -0,0 +1,103 @@
+// with dfs
+var maxAreaOfIslandz = function (grid) {
+  let maxCount = 0
+
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[0].length; y++) {
+      if (grid[x][y] !== 0) {
+        maxCount = Math.max(maxCount, dfs(x, y, 0))
+      }
+    }
+  }
+
+  return maxCount
+
+  // -----------------------------------------------
+  function dfs(i, j, count) {
+    if (grid[i][j] !== 0) {
+      grid[i][j] = 0
+      count = count + 1
+    }
+
+    const neighbors = getNeighbors(i, j, grid)
+
+    for (const neighbor of neighbors) {
+      const [nRow, nCol] = neighbor
+
+      if (grid[nRow][nCol] !== 0) count = dfs(nRow, nCol, count)
+    }
+    return count
+  }
+}
+
+// -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
+// with bfs
+var maxAreaOfIsland = function (grid) {
+  let maxCount = 0
+
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[0].length; y++) {
+      if (grid[x][y] !== 0) {
+        // Don't need to pass count as param like if dfs.
+        maxCount = Math.max(maxCount, bfs(x, y))
+      }
+    }
+  }
+
+  return maxCount
+
+  // -----------------------------------------------
+  function bfs(i, j) {
+    const queue = []
+    queue.push([i, j])
+    grid[i][j] = 0
+
+    let count = 1
+
+    while (queue.length) {
+      const [row, col] = queue.shift(),
+        neighbors = getNeighbors(row, col, grid)
+
+      for (const neighbor of neighbors) {
+        const [nRow, nCol] = neighbor
+
+        if (grid[nRow][nCol] !== 0) {
+          count++
+          queue.push([nRow, nCol])
+          grid[nRow][nCol] = 0
+        }
+      }
+    }
+    return count
+  }
+}
+
+// -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
+// Common
+function getNeighbors(x, y, grid) {
+  const result = []
+
+  if (x + 1 < grid.length) result.push([x + 1, y])
+  if (y + 1 < grid[0].length) result.push([x, y + 1])
+  if (x - 1 >= 0) result.push([x - 1, y])
+  if (y - 1 >= 0) result.push([x, y - 1])
+
+  return result
+}
+
+// tests
+const grid = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+]
+
+const grid1 = [[0, 0, 0, 0, 0, 0, 0, 0]]
+
+console.log(maxAreaOfIsland(grid))
+console.log(maxAreaOfIsland(grid1))
